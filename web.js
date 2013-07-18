1b260c3360f7e15760ebfcd8b9b1c1ed81b6713c
Vivek Thadani <vrt@outlook.com>
2013-07-18 10:52:31 +0000

    updated web.js again 3

--- web.js
+++ web.js
@@ -7,8 +7,12 @@ var fs = require('fs');
 var buf = new buffer(fs.readFileSync("index.html"));
 
 app.get('/', function(request, response) {
+
+
 response.send(buffer.toString('utf-8'));
-rs.});
+
+
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
